2c0fe491415c76cb8cf2c264eff59a95d7319ec1
afreedi <afreedisulfiker@gmai.com>
2022-09-25 22:56:29 +0530

    env problem solved

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
+require('dotenv').config({path:"./vars/.env"});
 const hbs = require('express-handlebars')
 const PORT = process.env.PORT || 4000;
 const route = require('./routes');
